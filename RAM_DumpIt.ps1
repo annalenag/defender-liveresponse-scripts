7407bec7eae236203db184b7db292bee6d621c7d
annalenag <159128053+annalenag@users.noreply.github.com>
2024-08-23 15:35:34 +0200

    Create RAM_DumpIt.ps1
    Initial commit.

---
+++ RAM_DumpIt.ps1
@@ -0,0 +1,40 @@
+<#
+	.NOTES
+	RAM_DumpIt.ps1
+	Anna Lena G.
+	v1.0
+	
+	.SYNOPSIS
+	Script creates RAM dump with DumpIt.exe from the Defender Live Response console.
+	
+	.EXAMPLE
+	PS> put RAM_DumpIt.ps1 -overwrite
+	PS> run RAM_DumpIt.ps1
+	
+#>
+Write-Host "
+RAM dump with DumpIt, v1.0
+
+Working...
+"
+
+$output_path = "C:\temp\response"
+$filename = "RAM_DUMP--" + $env:computername + "--" + (get-date -f dd-MM-yyyy-hhmm) + ".dmp"
+
+Write-Host -fore Green "
+
+RAM Dump:
+"
+
+# Create output path if it doesn't exist
+if (-Not (Test-Path -Path $output_path)) {
+  New-Item -Path $output_path -ItemType Directory
+}
+
+./DumpIt.exe /quiet /NOCOMPRESS /output $output_path\$filename
+
+Write-Host "
+
+RAM dump saved into:
+$output_path\$filename
+"
